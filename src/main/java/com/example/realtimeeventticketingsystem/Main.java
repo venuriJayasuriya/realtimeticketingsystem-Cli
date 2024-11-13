10076451618cad08aa858eac9577a70952c7f21e
venurij <venuri.20232405@iit.ac.lk>
2024-11-13 17:12:40 +0530

    validated CLI

--- src/main/java/com/example/realtimeeventticketingsystem/Main.java
+++ src/main/java/com/example/realtimeeventticketingsystem/Main.java
@@ -1,25 +1,10 @@
 package com.example.realtimeeventticketingsystem;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Realtime Event Ticketing System!");
-        prompts();
-    }
+        System.out.println("Welcome to Real-time Event Ticketing System!");
+        systemCLI.prompts();
 
-    public static void prompts() {
-        Scanner scanner = new Scanner(System.in);
-        systemCLI ticketInfo = new systemCLI();
-        System.out.println("Please enter the total number of tickets for the event:");
-        ticketInfo.setTotalTickets(scanner.nextInt());
-        System.out.println("Please enter the rate of ticket release for the event:");
-        ticketInfo.setTicketReleaseRate(scanner.nextDouble());
-        System.out.println("Please enter the customer retrieval rate for the event:");
-        ticketInfo.setCustomerRetrievalRate(scanner.nextDouble());
-        System.out.println("Please enter the max number of ticket capacity for the event:");
-        ticketInfo.setMaxTicketCapacity(scanner.nextInt());
-        System.out.println(ticketInfo);
     }
 }
--- src/main/java/com/example/realtimeeventticketingsystem/systemCLI.java
+++ src/main/java/com/example/realtimeeventticketingsystem/systemCLI.java
@@ -1,22 +1,34 @@
 package com.example.realtimeeventticketingsystem;
 
+import java.util.Scanner;
+
 public class systemCLI {
     private int totalTickets;
     private double ticketReleaseRate;
     private double customerRetrievalRate;
     private int maxTicketCapacity;
 
-    public systemCLI() { }
+    public systemCLI() {
 
+    }
 
+    public static void prompts() {
+        Scanner scanner = new Scanner(System.in);
+        systemCLI ticketInfo = new systemCLI();
+
+        System.out.println("Please enter the total number of tickets for the event:");
+        ticketInfo.setTotalTickets(intValidator(scanner));
+        System.out.println("Please enter the rate of ticket release for the event:");
+        ticketInfo.setTicketReleaseRate(doubleValidator(scanner));
+        System.out.println("Please enter the customer retrieval rate for the event:");
+        ticketInfo.setCustomerRetrievalRate(doubleValidator(scanner));
+        System.out.println("Please enter the max number of ticket capacity for the event:");
+        ticketInfo.setMaxTicketCapacity(intValidator(scanner));
+
+        System.out.println(ticketInfo);
+    }
     public void setTotalTickets(int totalTickets) {
-        if (totalTickets >= 0) {
-            this.totalTickets = totalTickets;
-        }
-        else {
-            System.out.println("Total tickets cannot be negative");
-            Main.prompts();
-        }
+        this.totalTickets = totalTickets;
     }
     public void setTicketReleaseRate(double ticketReleaseRate) {
         this.ticketReleaseRate = ticketReleaseRate;
@@ -39,6 +51,40 @@ public class systemCLI {
     }
     public int getMaxTicketCapacity() {
         return maxTicketCapacity;
+    }
+
+    private static int intValidator(Scanner scanner) {
+        while(true){
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt();
+                if (input >= 0) {
+                    return input;
+                } else {
+                    System.out.println("Please enter a positive integer");
+                }
+            }
+            else {
+                System.out.println("Invalid input. Please enter a positive integer.");
+                scanner.next();
+            }
+        }
+    }
+
+    private static double doubleValidator(Scanner scanner) {
+        while(true){
+            if (scanner.hasNextDouble()) {
+                double input = scanner.nextDouble();
+                if (input > 0) {
+                    return input;
+                } else {
+                    System.out.println("Please enter a positive integer");
+                }
+            }
+            else {
+                System.out.println("Invalid input. Please enter a positive integer.");
+                scanner.next();
+            }
+        }
     }
 
     @Override
